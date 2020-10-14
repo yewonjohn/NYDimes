cab26c9c1f6c7ebb05cb898a52ff34077cafacd8
johnkim <45464860+yewonjohn@users.noreply.github.com>
2020-10-14 10:25:17 -0400

    Initial Commit

---
+++ NYDimes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NYDimes
+//
+//  Created by John Kim on 10/14/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
